6d97cf5130f44c9d5653a98dadd608cae83dd0d1
MoatazAhmedZaki <moatazcs@gmailcom>
2021-03-13 18:25:07 +0200

    Deploy

---
+++ js/index.js
@@ -0,0 +1,49 @@
+
+/*
+number
+string
+boolean
+undefined
+null
+
+object
+
+alert("rest")
+var username = "ahmed";
+
+console.log(username)
+
+alert("welcome : "+username)
+
+var x = 5;
+var y = "5";
+
+if(x === y)
+    {
+        console.log("sa7");
+    }
+else
+    {
+        console.log("8alat");
+    }
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
